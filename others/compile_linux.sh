e371bc45d3f230fb8d5e0fe645f3afc00591efa5
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2019-07-24 22:44:39 +0900

    delete others

--- others/compile_linux.sh
+++
@@ -1,6 +0,0 @@
-g++ -std=c++11 -Wall -shared -fPIC \
-  -I pybind11/include -I marlib \
-  `python3-config --cflags --ldflags` \
-  nmarkov/src/nmarkov.cpp \
-  -o nmarkov/_nmarkov`python3-config --extension-suffix` \
-  -l blas
--- others/run.sh
+++
@@ -1,2 +0,0 @@
-docker build -t jupyter/z3 .
-docker run -it --name notebook --rm -p 8888:8888 -v $(pwd):/home/jovyan/work jupyter/z3
